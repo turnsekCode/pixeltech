fd2bb18002fe9e7d83f814fdb4fd6f1db94dda39
abraham <abraham.turnsek@gmail.com>
2024-04-30 11:52:34 +0200

    creacion de .env

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -7,24 +7,22 @@ export async function POST(req: NextApiRequest, res: NextApiResponse) {
   let bodyreq = JSON.parse(passValue);
   const { name, email, subject, message } = bodyreq;
 
-
-console.log("bodyconsole",email)
   // Configura el transporte de correo electrónico
   const transporter = nodemailer.createTransport({
     // Configura tu proveedor de correo electrónico aquí
     // Por ejemplo, puedes usar Gmail, Outlook, etc.
     service: 'Gmail',
     auth: {
-      user: 'pixel.tech.t@gmail.com',
-      pass: 'qlll zjkj mgvx jjld'
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_PASS
     }
   });
 
   try {
     // Envía el correo electrónico
     await transporter.sendMail({
-      from: 'pixel.tech.t@gmail.com',
-      to: "pixel.tech.t@gmail.com",
+      from: process.env.MAIL_USER,
+      to: process.env.MAIL_USER,
       subject: subject,
       text: `Nombre: ${name}\nCorreo: ${email}\n Mensaje: ${message}`
     });
