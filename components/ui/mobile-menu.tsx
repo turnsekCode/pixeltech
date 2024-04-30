50cdabddfacf99c56eddfb57d1328139e28da607
abraham <abraham.turnsek@gmail.com>
2024-04-30 19:39:09 +0200

    correccion en menu movil y añadido de sitemap

--- components/ui/mobile-menu.tsx
+++ components/ui/mobile-menu.tsx
@@ -47,7 +47,7 @@ export default function MobileMenu() {
         <svg className="w-6 h-6 fill-current text-gray-900" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
           <rect y="4" width="24" height="2" />
           <rect y="11" width="24" height="2" />
-          <rect y="18" width="24" height="2" />
+          <rect y="12" width="24" height="2" />
         </svg>
       </button>
 
