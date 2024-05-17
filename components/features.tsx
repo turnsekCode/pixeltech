119c3069a4f3d4e5dccd4f1ccd1e1e63038842a4
abraham <abraham.turnsek@gmail.com>
2024-05-17 20:03:58 +0200

    actualizacion de proyectos en home

--- components/features.tsx
+++ components/features.tsx
@@ -22,27 +22,28 @@ export default function Features() {
 
               {/* Item 1 */}
               <div style={{background: "linear-gradient(180deg,#f8f8f8,hsla(0,0%,100%,0)"}} className="mt-4 max-w-xl md:max-w-none md:w-full mx-auto md:col-span-6 lg:col-span-6 md:mt-6 bg-white rounded-2xl shadow-xl overflow-hidden" data-aos="fade-right">
+                <div style={{backgroundImage: "url('/dashboard1.png')", backgroundPosition: "center", backgroundSize: "cover"}} className="h-60 rounded-t-2xl transition-transform duration-700 hover:scale-110"></div>
+                <div className="h-30 p-6"><a href='https://admin.isolution.site/login' target='_blank' className="text-xl font-semibold leading-snug tracking-tight mb-1">Panel administrativo pixeltech</a><p className="text-gray-600 mb-2">Aplicación web creada con Reactjs y nodejs</p><Link className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" href='/proyectos/desarrollo-web-dashboard-pixeltech'>Ver detalles</Link></div>
+              </div>
+
+               {/* Item 2 */}
+               <div style={{background: "linear-gradient(180deg,#f8f8f8,hsla(0,0%,100%,0)"}} className="mt-4 max-w-xl md:max-w-none md:w-full mx-auto md:col-span-6 lg:col-span-6 md:mt-6 bg-white rounded-2xl shadow-xl overflow-hidden" data-aos="fade-right">
                 <div style={{backgroundImage: "url('/quickgold1.png')", backgroundPosition: "center", backgroundSize: "cover"}} className="h-60 rounded-t-2xl transition-transform duration-700 hover:scale-110"></div>
                 <div className="h-30 p-6"><a href='https://quickgold.es/' target='_blank' className="text-xl font-semibold leading-snug tracking-tight mb-1">quickgold.es</a><p className="text-gray-600 mb-2">Aplicación web creada con Nextjs y nodejs</p><Link className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" href='/proyectos/desarrollo-web-quickgold'>Ver detalles</Link></div>
               </div>
 
-              {/* Item 2*/}
+              {/* Item 3*/}
               <div style={{background: "linear-gradient(180deg,#f8f8f8,hsla(0,0%,100%,0)"}} className="mt-4 max-w-xl md:max-w-none md:w-full mx-auto md:col-span-6 lg:col-span-6 md:mt-6 bg-white rounded-2xl shadow-xl overflow-hidden" data-aos="fade-left">
                 <div style={{backgroundImage: "url('/mockup_app.png')", backgroundPosition: "center", backgroundSize: "cover"}} className="h-60 rounded-t-2xl transition-transform duration-700 hover:scale-110"></div>
                 <div className="h-30 p-6"><a href='https://quickgold.es/app' target='_blank' className="text-xl font-semibold leading-snug tracking-tight mb-1">App móvil quickgold</a><p className="text-gray-600 mb-2">Aplicación móvil híbrida creada con React native y nodejs</p><Link className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" href='/proyectos/app-movil-quickgold'>Ver detalles</Link></div>
               </div>
 
-              {/* Item 3*/}
+              {/* Item 4*/}
               <div style={{background: "linear-gradient(180deg,#f8f8f8,hsla(0,0%,100%,0)"}} className="mt-4 max-w-xl md:max-w-none md:w-full mx-auto md:col-span-6 lg:col-span-6 md:mt-6 bg-white rounded-2xl shadow-xl overflow-hidden" data-aos="fade-right">
               <div style={{backgroundImage: "url('/sorteo.quickgol1.png')", backgroundPosition: "center", backgroundSize: "cover"}} className="h-60 rounded-t-2xl transition-transform duration-700 hover:scale-110"></div>
                 <div className="h-30 p-6"><a href='https://sorteo.pixeltech.es/' target='_blank' className="text-xl font-semibold leading-snug tracking-tight mb-1">sorteo.pixeltech.es</a><p className="text-gray-600 mb-2">Página web creada con Nextjs y nodejs</p><Link className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" href='/proyectos/desarrollo-web-sorteo-quickgold'>Ver detalles</Link></div>
               </div>
 
-              {/* Item 4*/}
-              <div style={{background: "linear-gradient(180deg,#f8f8f8,hsla(0,0%,100%,0)"}} className="mt-4 max-w-xl md:max-w-none md:w-full mx-auto md:col-span-6 lg:col-span-6 md:mt-6 bg-white rounded-2xl shadow-xl overflow-hidden" data-aos="fade-left">
-              <div style={{backgroundImage: "url('/currency1.png')", backgroundPosition: "center", backgroundSize: "cover"}} className="h-60 rounded-t-2xl transition-transform duration-700 hover:scale-110"></div>
-                <div className="h-30 p-6"><a href='https://currency.pixeltech.es' target='_blank' className="text-xl font-semibold leading-snug tracking-tight mb-1">currency.pixeltech.es</a><p className="text-gray-600 mb-2">Página web creada con nextjs</p><Link className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" href='/proyectos/desarrollo-web-currency'>Ver detalles</Link></div>
-              </div>
             </div>
             <div className="lg:w-full flex justify-center items-center mt-16"><Link className="btn w-36 text-white bg-blue-600 hover:bg-blue-700 sm:w-auto sm:mb-0" href='/proyectos'>Ver todos</Link></div>
           </div>
